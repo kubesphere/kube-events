310277f6756a5d4de0efc72c28c12df000c51764
junot <junotxiang@yunify.com>
2021-08-16 17:34:01 +0800

    upgrade webhook api version to v1
    Signed-off-by: junot <junotxiang@yunify.com>

--- hack/certs.sh
+++ hack/certs.sh
@@ -57,11 +57,15 @@ echo "creating certs in certsdir ${CERTSDIR} "
 openssl genrsa -out ${CERTSDIR}/ca.key 2048
 
 # create ca.crt
-openssl req -x509 -new -nodes -key ${CERTSDIR}/ca.key -subj "/C=CN/ST=HB/O=QC/CN=${service}" -sha256 -days 10000 -out ${CERTSDIR}/ca.crt
+openssl req -x509 -new -nodes -key ${CERTSDIR}/ca.key -subj "/C=CN/ST=HB/O=QC/CN=${service}" -sha256 -days 3650 -out ${CERTSDIR}/ca.crt
 
 # create server.key
 openssl genrsa -out ${CERTSDIR}/server.key 2048
 
 # create server.crt
-openssl req -new -sha256 -key ${CERTSDIR}/server.key -subj "/C=CN/ST=HB/O=QC/CN=${service}.${namespace}.svc" -out ${CERTSDIR}/server.csr
-openssl x509 -req -in ${CERTSDIR}/server.csr -CA ${CERTSDIR}/ca.crt -CAkey ${CERTSDIR}/ca.key -CAcreateserial -out ${CERTSDIR}/server.crt -days 10000 -sha256
+openssl req -new -sha256 -key ${CERTSDIR}/server.key -subj "/C=CN/ST=HB/O=QC/CN=${service}.${namespace}.svc" -out ${CERTSDIR}/server.csr \
+  -reqexts SAN -config <(cat /etc/ssl/openssl.cnf \
+    <(printf "[SAN]\nsubjectAltName=DNS:${service},DNS:${service}.${namespace},DNS:${service}.${namespace}.svc"))
+openssl x509 -req -in ${CERTSDIR}/server.csr -CA ${CERTSDIR}/ca.crt -CAkey ${CERTSDIR}/ca.key -CAcreateserial -out ${CERTSDIR}/server.crt -days 3650 -sha256 \
+  -extensions SAN -extfile <(cat /etc/ssl/openssl.cnf \
+    <(printf "[SAN]\nsubjectAltName=DNS:${service},DNS:${service}.${namespace},DNS:${service}.${namespace}.svc"))
